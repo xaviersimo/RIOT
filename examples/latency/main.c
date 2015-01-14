f401b49936a3eb02a2a4d5215e7ff2b8b212693f
root <xavier.simo@triagnosys.com>
2015-01-14 04:50:51 -0500

    upload the stadistic values and add # in all printf

--- examples/latency/main.c
+++ examples/latency/main.c
@@ -26,18 +26,38 @@
 #include "thread.h"
 #include "msg.h"
 
+#define MULTI_THREAD    (0)
+
+#if MULTI_THREAD
+char stack[20][KERNEL_CONF_STACKSIZE_MAIN];
+#endif
+
 #define MSEC (1000)
 #define SEC (1000 * MSEC)
+#define MAX_LATENCY 1000
 
-int latency[1000] = {0}; /* define vector for latency */
-int count[1000] = {0};
+int latency[MAX_LATENCY] = {0}; /* define vector for latency */
+int count[MAX_LATENCY] = {0};
+
+#if MULTI_THREAD
+void *second_thread(void *arg)
+{
+	(void) arg;
+
+	while (1) {
+
+	}
+
+	return NULL;
+}
+#endif
 
 int main(void)
 {
 
 
 	/*vector variable*/
-	int index = 1000;
+	int overflow = 0;
 	int j = 0;
 	int c = 0;
 
@@ -52,7 +72,7 @@ int main(void)
 
 	/*inizialize parameters*/
 	int iteration = 0;
-	int test_repeats = 1000;
+	int test_repeats = 10000;
 
 	/*print values*/
 	int n = 0;
@@ -60,34 +80,57 @@ int main(void)
 	/*Maximum and minimum values*/
 	int max_c = count[0];
 	int max_l = latency[0];
-	int inc_min = 0;
-	int min_c[100] = {0};
-	int min_l[100] = {0};
-	int print_min = 0;;
+	int max_time_c = count[0];
+	int max_time_l = latency[0];
+	int min_time_c = 0;
+	int min_time_l = 0;
+	int min_time_flag = 1;
 
 
+#if MULTI_THREAD
+	/*Define multiple threads*/
+	int threads=6;
+	int th=1;
+	/*define multi sleeping thread*/
+	kernel_pid_t pid[threads];
+	const char buffer[threads][10];
+
+for(th=1 ; th < threads +1; th++)
+	{
+	sprintf(buffer[th], "th_back_%d", th);
+	//printf("buffer is:%s\n", buffer[th]);
+	pid[th] = thread_create(stack[th],
+			KERNEL_CONF_STACKSIZE_MAIN,
+			PRIORITY_MAIN - th,
+			CREATE_WOUT_YIELD | CREATE_STACKTEST | CREATE_SLEEPING,
+			second_thread,
+			NULL,
+			buffer[th]);
+	}
+
+#endif
 
 
 	/*Init program*/
-	printf("********************************************* \n");
-	printf("************* Latency RIOT test ************* \n");
-	printf("********************************************* \n");
-	printf("config parameters:\n\n");
-	printf("Interval sleep: %"PRIu32" sec and %"PRIu32" micro\n", interval.seconds, interval.microseconds);
-	printf("Samples: %i\n", index);
-	printf("time process: %i\n", test_repeats);
-	printf("********************************************* \n");
-	printf("\n \n");
-	vtimer_usleep(SEC);
+	printf("# ********************************************* \n");
+	printf("# ************* Latency RIOT test ************* \n");
+	printf("# ********************************************* \n");
+	printf("# config parameters:\n");
+	printf("# Interval sleep: %"PRIu32" sec and %"PRIu32" micro\n", interval.seconds, interval.microseconds);
+	printf("# Samples: %i\n", MAX_LATENCY);
+	printf("# Repetitions: %i\n", test_repeats);
+	printf("# ********************************************* \n");
 
 	/*Init latency vector*/
 
-	for(j = 0 ; j < index; j++) {
+	for(j = 0 ; j < MAX_LATENCY; j++) {
 		latency[j] = c;
 		c += 1;
 	}
 
-	printf("vector init\n");
+	printf("# vector init\n");
+
+	vtimer_usleep(SEC);
 
 
 	vtimer_now(&now);
@@ -98,13 +141,16 @@ int main(void)
 			vtimer_now(&now); // get actual time after sleep (:=now)
 			diff = timex_sub(now, next); // compute difference between theoretical time after sleep (:=next)
                                          // and actual time after sleep (:=now)
-			next = timex_add(interval, now); // update theoretical time for next iteration
 
 			iteration++;
 
-			if (diff.microseconds > 99999) // guard for overflow
-				diff.microseconds = 0x100000000 - diff.microseconds;
-			count[diff.microseconds] += 1; // store diff result for statistics
+			if (diff.microseconds > MAX_LATENCY - 1) // guard for overflow
+				overflow++;
+			else
+				count[diff.microseconds] += 1; // store diff result for statistics
+
+			vtimer_now(&now); // get actual time for next iteration (:=now)
+			next = timex_add(interval, now); // update theoretical time for next iteration
 
 			//printf("the now.microseconds is: %"PRIu32"\n", now.microseconds);
 			//printf("the next.microseconds is: %"PRIu32"\n", next.microseconds);
@@ -114,12 +160,12 @@ int main(void)
 
 		if(test_repeats == iteration) {
 			test_repeats = 0;
-			printf("Test finish\n");
-			printf("print histogram\n");
+			printf("# Test finish\n");
+			printf("# print histogram\n");
 			vtimer_usleep(SEC);
 
 			/*print out values*/
-			for(n = 0; n < index ; n++) {
+			for(n = 0; n < MAX_LATENCY ; n++) {
 				if(n < 10)
 					printf("00%i %i\n", latency[n], count[n]);
 				else if(n<100 && n>=10)
@@ -127,24 +173,42 @@ int main(void)
 				else
 					printf("%i %i\n", latency[n], count[n]);
 
-				/*Get the maximum values*/
+				/*Get the maximum repetitions*/
 				if (max_c < count[n]) {
 					max_c = count[n];
 					max_l = latency[n];
 				}
 
-				/*Get the minimum values*/
-				if ( count[n] == 1) {
-					inc_min ++;
-					min_c[inc_min] = count[n];
-					min_l[inc_min] = latency[n];
+				/*Get time values*/
+				if (count[n] >= 1){
+					/*Get maximum time value*/
+					if(max_time_l < latency[n]) {
+						max_time_l = latency[n];
+						max_time_c = count[n];
+					}
+					/*Get minimum time value*/
+					if(min_time_flag){
+						min_time_flag = 0;
+						min_time_l = latency[n];
+						min_time_c = count[n];
+					}
 				}
 			}
-			printf("MAX: %i microsec in %i repetitions \n\n", max_l, max_c);
-			for(print_min = 1; print_min <= inc_min; print_min++){
-				printf("MIN: %i microsec in %i repetitions\n", min_l[print_min], min_c[print_min]);
+			printf("# MIN time is: %i microseconds in %i repetitions\n", min_time_l, min_time_c);
+			printf("# MAX time is: %i microseconds in %i repetitions\n", max_time_l, max_time_c);
+			printf("# overflow is: %i\n", overflow);
+			printf("# MAX repetitions are: %i repetitions in %i microseconds\n", max_c, max_l);
 
-			}
+
+				//printf("# MIN: %i microsec in %i repetitions\n", min_l[print_min], min_c[print_min]);
+
+
+
+#if MULTI_THREAD
+			printf("\n\n");
+			thread_print_all();
+#endif
+			LED_GREEN_OFF; //indicate test finish
 		}
 	}
 
